ad8b2095b609d1803065cc05997828a4639a9ee6
‘cauchy221’ <1321378776@qq.com>
2022-06-03 10:46:40 +0800

    version 1 by iris

---
+++ finetune.sh
@@ -0,0 +1,13 @@
+accelerate launch finetune.py \
+ --model_path bert-base-uncased \
+ --output_dir "output/baseline_lr5e-5" \
+ --train_file "data/preprocessed/train.csv" \
+ --validation_file "data/preprocessed/dev.csv" \
+ --test_file "data/preprocessed/test.csv" \
+ --per_device_batch_size 8 \
+ --num_train_epochs 10 \
+ --learning_rate 5e-5 \
+ --num_warmup_steps 74 \
+ --seed 42 \
+ --experiment_name sentiment_analysis \
+ --scenario_name "batch_size 8, lr 5e-5, baseline"
